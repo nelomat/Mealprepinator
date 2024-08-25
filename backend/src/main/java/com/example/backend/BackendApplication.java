85fdedb4ab61831fc64d269fa07adfc2907b5280
Nele <nelegierke77@gmail.com>
2024-08-25 11:04:12 +0200

    Backend WIP

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -1,5 +1,6 @@
 package com.example.backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+    Dotenv dotenv = Dotenv.configure().load();
+    SpringApplication.run(BackendApplication.class, args);
 	}
-
 }
---
+++ backend/src/main/java/com/example/backend/Repository/IngredientRepository.java
@@ -0,0 +1,9 @@
+package com.example.backend.Repository;
+
+import com.example.backend.Model.Ingredient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
+}
