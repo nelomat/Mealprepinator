8ab70aacfab05776af47ae261c1e10101bb7da9d
Nele <nelegierke77@gmail.com>
2024-08-28 18:51:32 +0200

    Backend WIP

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -1,14 +1,10 @@
 package com.example.backend;
 
-import io.github.cdimascio.dotenv.Dotenv;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
-    Dotenv dotenv = Dotenv.configure().load();
-    SpringApplication.run(BackendApplication.class, args);
 	}
 }
