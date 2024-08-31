4e17382bc9237872627b7a03d7c44d80d0216de0
Nele <nelegierke77@gmail.com>
2024-08-31 13:07:59 +0200

    add DatabaseInitializer and Sample Data via SQL Script

--- backend/src/main/java/com/example/backend/Controller/RecipeController.java
+++ backend/src/main/java/com/example/backend/Controller/RecipeController.java
@@ -3,6 +3,7 @@ package com.example.backend.Controller;
 import com.example.backend.Model.Recipe;
 import com.example.backend.Service.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,9 +19,18 @@ public class RecipeController {
   private RecipeService recipeService;
 
   @GetMapping
-  public List<Recipe> getAllRecipes() {
-    return recipeService.getAllRecipes();
+  public ResponseEntity<?> getAllRecipes() {
+    try {
+      List<Recipe> recipes = recipeService.getAllRecipes();
+      if (recipes.isEmpty()) {
+        return new ResponseEntity<>("No recipes found. ", HttpStatus.NOT_FOUND);
+      }
+      return new ResponseEntity<>(recipes, HttpStatus.OK);
+    } catch (Exception e) {
+      return new ResponseEntity<>("Get recipes Server Error  " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
+
 
   @PostMapping
   public Recipe createRecipe(@RequestBody Recipe recipe) {
